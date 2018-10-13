366e2d50146a4b65905ff8b7f6d461458e53d4bd
Vinod <Vinod.Ramakrishnan@honeywell.com>
2018-10-13 12:47:41 +0530

    Refactor into a method

--- roman.groovy
+++ roman.groovy
@@ -1,9 +1,11 @@
 roman_numbers = ["I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000]
 
 translate = { roman_number -> 
-  roman_number.inject(0) { total, roman_value -> 
-      (total + roman_numbers[roman_value]) - 2 * (total % roman_numbers[roman_value])
-    }
+  roman_number.inject(0, add_roman)
+}
+
+add_roman = { total, roman_value -> 
+   (total + roman_numbers[roman_value]) - 2 * (total % roman_numbers[roman_value])
 }
 
 assert translate('MMMCM') == 3900
